1eed2aadfdeca20007266b6a3a8e29288c3f2c4d
IM-NOT-PHP-PROGRAMMER <35314850+IM-NOT-PHP-PROGRAMMER@users.noreply.github.com>
2018-11-07 23:42:14 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,36 @@
 #!/usr/bin/python
-import time
+import socket
 
 if __name__ == '__main__':
+    host = ''
+    port = 8080
+    socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    socket.bind((host, port))
+    socket.listen(10)
     while True:
-        try:
-            file = open('notemptyfile.txt')
-            print(file.read())
-            file.close()
-        except Exception:
-            print('smth gone wrong')
-        time.sleep(5)
+        clientSocket, addr = socket.accept()
+
+        clientRequest = str(clientSocket.recv(1024))
+        clientPath = clientRequest.split(" ")[1]
+        print("Connection from {addr} path: {clientPath}".format(addr=addr, clientPath=clientPath))
+
+        if not "?" in clientPath or clientPath[:clientPath.index("?")] != "/proceed" or not "?e=" in clientPath or "&" in clientPath[(clientPath.index("?") + 3):]:
+            clientSocket.close()
+            continue
+
+        e = clientPath[(clientPath.index("?") + 3):]
+
+        clientSocket.sendall(bytes("HTTP/1.0 200 OK\n"
+                                   "Content-Type: text/html\n"
+                                   "Connection: Close\n"
+                                   "Access-Control-Allow-Origin: *\r\n"
+                                   "\r\n"
+                                   "<html>\n"
+                                   "<head>\n"
+                                   "<title>Success</title>\n"
+                                   "</head>\n"
+                                   "<body>\n"
+                                   "Boo!\n"
+                                   "</body>\n"
+                                   "</html>\n", "UTF-8"))
+        clientSocket.close()
